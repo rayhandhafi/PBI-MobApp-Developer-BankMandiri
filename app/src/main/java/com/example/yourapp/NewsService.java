d62bc6e383f992132d7b8901f056c828452a2628
ThumbsUp <80929874+rayhandhafi@users.noreply.github.com>
2024-06-03 20:02:10 +0700

    Create NewsService.java

---
+++ app/src/main/java/com/example/yourapp/NewsService.java
@@ -0,0 +1,10 @@
+package com.example.yourapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsService {
+    @GET("top-headlines")
+    Call<NewsResponse> getTopHeadlines(@Query("country") String country, @Query("apiKey") String apiKey);
+}
